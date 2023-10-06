3fbedfa969c4bb31beabb83857939a6048e456f6
Alessandro Su <t.su1@studenti.unisa.it>
2023-10-06 23:50:11 +0200

    ristorazione del vecchio modello

--- WorldCrafters2/src/main/java/it/unisa/utils/DriverManagerConnectionPool.java
+++ WorldCrafters2/src/main/java/it/unisa/utils/DriverManagerConnectionPool.java
@@ -25,7 +25,7 @@ public class DriverManagerConnectionPool  {
 		String port = "3306";
 		String db = "worldcrafters";
 		String username = "root";
-		String password = "1234";
+		String password = "Sutianyu02.";
 
 		newConnection = DriverManager.getConnection("jdbc:mysql://"+ ip+":"+ port+"/"+db+"?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", username, password);
 		newConnection.setAutoCommit(false);
